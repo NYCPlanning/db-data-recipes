f6705f8523a6af12b38facb584aa0aa004ed9dfa
sptkl <caobaiyue@gmail.com>
2019-05-22 15:24:39 -0400

    update settings.example.sh

--- settings.example.sh
+++ settings.example.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-## environmental variables settings
-export AWS_ACCESS_KEY_ID='XXXXXXXXXXXXXXX'
-export AWS_SECRET_ACCESS_KEY='XXXXXXXXXXX'
-export S3_ENDPOINT_URL='XXXXXXXXXXXXXXXXX'
-export DPP_DB_ENGINE='XXXXXXXXXXXXXXXXXXX'
-export DATAFLOWS_DB_ENGINE='XXXXXXXXXXXXX'
-export BUCKET='BUCKETXXXXXXXXXXXXXXXXXXXX'
-
 ## install custom python pacakges
-pip install -e lib/
+pip install -e .
 
 ## settings for cli
 eval "$(_COOK_COMPLETE=source cook)"
