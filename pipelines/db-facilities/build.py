44bbe86cea3f9fc1581169ec634a1905b53da04d
Baoling Zhou <baolingz@nyu.edu>
2019-06-06 11:50:18 -0400

    update recipe for nysdec_lands

--- pipelines/db-facilities/build.py
+++ pipelines/db-facilities/build.py
@@ -59,6 +59,11 @@ def ETL():
         Load('dot_ferryterminals', 'latest'),
         Load('dot_publicparking', 'latest'),
         Load('hra_centers', 'latest'),
+<<<<<<< HEAD
+        Load('nysdec_lands', 'latest'),
+=======
+        Load('nycha_communitycenters', 'latest'),
+>>>>>>> eeccc82... add recipe for nycha_communitycenters
         dump_2_s3(params=dict(base_path=base_path))
     ).process()
 
