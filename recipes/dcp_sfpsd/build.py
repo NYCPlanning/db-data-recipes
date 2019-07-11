0b093eb207e696796a04569f17973e741e8c7900
Baoling Zhou <baolingz@nyu.edu>
2019-07-11 10:15:22 -0400

    update recipe for dcp_sfpsd

--- recipes/dcp_sfpsd/build.py
+++ recipes/dcp_sfpsd/build.py
@@ -7,6 +7,7 @@ import requests
 from pathlib import Path
 import urllib.request
 import zipfile
+import pandas as pd
 
 def unzip(table_name):
     url = 'https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/facilities_csv_201901.zip'
@@ -27,7 +28,8 @@ def clean_up():
 
 def ETL(table_name):
     base_path = create_base_path(__file__)
-    file_path = get_csv()
+    file_path = Path(__file__).parent/'dcp_sfpsd.csv'
+
     Flow(
         load(str(file_path), name=table_name, format='csv', force_strings=True),
         joined_lower(resources=table_name),
@@ -37,5 +39,14 @@ def ETL(table_name):
 if __name__ == '__main__':
     table_name = 'dcp_sfpsd'
     unzip(table_name)
+    file_path = Path(__file__).parent/'tmp'/'facilities_20190110.csv'
+    df = pd.read_csv(file_path, dtype = 'str')
+    dcp_sfpsd = ['amtrak_facilities_sfpsd', 'bbpc_facilities_sfpsd',
+                'hrpt_facilities_sfpsd', 'mta_facilities_sfpsd',
+                'nysdot_facilities_sfpsd', 'panynj_facilities_sfpsd',
+                'tgi_facilities_sfpsd', 'dcas_facilities_colp',
+                'rioc_facilities_sfpsd'] 
+    df = df[df.pgtable.isin(dcp_sfpsd)]
+    df.to_csv(Path(__file__).parent/'dcp_sfpsd.csv')
     ETL(table_name)
     clean_up()
