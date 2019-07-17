a30d94045f92e81415fdc5f7ffabf26d25030dc4
Baoling Zhou <baolingz@nyu.edu>
2019-07-17 11:52:55 -0400

    Update recipe for nysdoccs_corrections

--- recipes/nysdoccs_corrections/build.py
+++ recipes/nysdoccs_corrections/build.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import ssl
 import json
+import pandas as pd
 
 def ETL(data):
     table_name = 'nysdoccs_corrections'
@@ -24,14 +25,16 @@ if __name__ == "__main__":
     site= 'http://www.doccs.ny.gov/faclist.html'
     soup = BeautifulSoup(requests.get(site).content, features='html.parser')
     data = []
-    for i in soup.find_all('tr'):
+    for i in soup.find_all('tr')[1:]:
         info = [item.strip() for item in i.get_text().split('\n') if item not in ['', 'Map', 'Driving Directions']]
         address_long = ', '.join(info[1:-2]).split(', (')[0]
+        county = (', '.join(info[1:-2]).split(', (')[1]).split('(')[1].split(' Co')[0]
         result = dict(
             facility_name = info[0],
             address = address_long,
             house_number = get_hnum(address_long),
-            street_name = get_sname(address_long), 
+            street_name = get_sname(address_long).replace(',',''), 
+            county = county,
             zipcode = get_zipcode(address_long),
             security_level = info[-2],
             male_or_female = info[-1]
